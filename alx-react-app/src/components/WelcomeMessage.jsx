450e821013a1ace1e91557776560adb1acd5e03e
Rasaq-ola <rasaqola196@gmail.com>
2025-09-01 19:04:29 +0100

    creating a react-app and and first component

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,13 +1,9 @@
-import WelcomeMessage from './components/WelcomeMessage;
-
-
 function WelcomeMessage() {
     return (
         <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>I am learning about JSX!</p>
         </div>
-	<WelcomeMessage />
     );
 }
 
