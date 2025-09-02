76147392913be1e46d5f3f468ecdb366a47dc57b
Rasaq-ola <rasaqola196@gmail.com>
2025-09-02 22:34:17 +0100

    adding a header, footer and maincontent file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,8 +32,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
-	<WelcomeMessage />
   )
 }
 
